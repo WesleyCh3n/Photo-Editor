ad413e147e253906476309258c748c9a3a5eaf72
avmm9898 <b04611002@ntu.edu.tw>
2017-08-21 11:37:48 +0800

    update mine

--- mainwindow.h
+++ mainwindow.h
@@ -25,10 +25,12 @@ public:
     ~MainWindow();
 public slots:
     QImage * MainWindow::greyScale(QImage * origin);
+    QImage * MainWindow::brightnessScale(bool,int,QImage * origin);
+    void undo_redo_enable();
 
 private slots:
 
-    void on_verticalSlider_valueChanged(int value);
+    void on_BrightnessSlider_valueChanged(int value);
 
     void on_greybtn_clicked();
 
@@ -40,12 +42,17 @@ private slots:
 
     void on_actionRedo_triggered();
 
+
+
+    void on_brightnessset_btn_clicked();
+
 private:
     Ui::MainWindow *ui;
     QPixmap pixmap;
-    QImage *Image[10];
-    QImage *I;
+    //QVector<QImage> *Image;   //可記錄100個步驟
+    QImage *Image[100];
     int currentstep=0;
+    int brightnessdelta=0;
 
 };
 
